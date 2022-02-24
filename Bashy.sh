71f720593188472f574153d132259267d2ea4287
JamerGamer <Jfriggs1877@gmail.com>
2022-02-23 20:17:21 -0500

    Added emotional support

--- Bashy.sh
+++ Bashy.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+echo "Why are you feeling sad?"
+
+read emotions
+
+echo "So you are sad because of "$emotions" (Answery Y/y or N/n)?"
+
+read answer
+if [ $answer == "y" ]
+
+then
+echo "loser"
+
+else
+echo "okay"
+
+fi 
+
